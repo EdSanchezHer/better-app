728ced2e1119d5dc883474fcdffe31503dc0bb30
Eduardo <eduardosh1105@gmail.com>
2021-06-22 10:54:02 -0600

    Added migrations for Q&As

---
+++ backend/db/migrations/20210622163902-create-question.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable("Questions", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			ownerId: {
+				type: Sequelize.INTEGER,
+				references: { model: "Users" },
+			},
+			title: {
+				type: Sequelize.STRING(60),
+				allowNull: false,
+			},
+			description: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable("Questions");
+	},
+};
